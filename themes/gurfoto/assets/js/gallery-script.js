29934fc28e15bef7eaf718d5497768cdeb01c30f
Nikolay Kolomyytsev <kolya.ya@gmail.com>
2024-10-29 20:18:04 +0100

    feat: added scripts and styles for gellery prview dialog

--- themes/gurfoto/assets/js/gallery-script.js
+++ themes/gurfoto/assets/js/gallery-script.js
@@ -1,4 +1,4 @@
-console.log('gallery-script.js loaded');
+console.log("gallery-script.js loaded");
 // const picArr = []
 // const galleries = document.querySelectorAll('.sc-gallery');
 // galleries.forEach((gallery) => {
@@ -10,23 +10,70 @@ console.log('gallery-script.js loaded');
 
 // console.log(picArr);
 
-const galleries = document.querySelectorAll('.sc-gallery');
-const dialog = document.querySelector('.sc-gallery__dialog');
-const dialogCloseBtn = dialog.querySelector('.sc-gallery__dialog-close');
-const dialogPic = dialog.querySelector('picture');
-const dialogLegacyImg = dialogPic.querySelector('img');
-
-dialogCloseBtn.addEventListener("click", () => {
-	dialog.close();
-})
+const galleries = document.querySelectorAll(".sc-gallery");
+const dialog = document.querySelector(".gallery-dialog");
+const dialogCloseBtn = dialog.querySelector(".gallery-dialog__close");
+const dialogPic = dialog.querySelector("picture");
+const dialogLegacyImg = dialogPic.querySelector("img");
+let scrollPosition = 0;
 
 galleries.forEach((gallery) => {
-    gallery.addEventListener("click", (e) => {
-        if (e.target.tagName === "IMG") {
-            console.log(e.target.dataset)
+	gallery.addEventListener("click", (e) => {
+		if (e.target.tagName === "IMG") {
+			console.log(e.target.dataset);
 			dialogLegacyImg.src = e.target.dataset.legacylink;
 			dialogLegacyImg.alt = e.target.alt;
-            dialog.showModal()
+			openDialog();
 		}
 	});
-})
+});
+
+dialogCloseBtn.addEventListener("click", () => {
+	closeDialog();
+});
+
+dialog.addEventListener("click", (e) => {
+	const dialogDimensions = dialog.getBoundingClientRect();
+	if (
+		e.clientX < dialogDimensions.left ||
+		e.clientX > dialogDimensions.right ||
+		e.clientY < dialogDimensions.top ||
+		e.clientY > dialogDimensions.bottom
+	) {
+		closeDialog();
+	}
+});
+
+dialog.addEventListener("cancel", (e) => {
+	closeDialog();
+	e.preventDefault();
+});
+
+// Prevent touchmove events on iOS when dialog is open
+document.addEventListener(
+	"touchmove",
+	(e) => {
+		if (dialog.open && !dialog.contains(e.target)) {
+			e.preventDefault();
+		}
+	},
+	{ passive: false },
+);
+
+function openDialog() {
+	scrollPosition = window.scrollY;
+	document.body.classList.add("dialog-open");
+	document.body.style.top = `-${scrollPosition}px`;
+	dialog.showModal();
+}
+
+function closeDialog() {
+	document.body.classList.remove("dialog-open");
+	document.body.style.top = "";
+	window.scrollTo({
+		left: 0,
+		top: scrollPosition,
+		behavior: "instant",
+	});
+	dialog.close();
+}
